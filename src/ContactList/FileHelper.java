690b94ccf804c58008373a67999c585afc291d51
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-08-19 00:08:57 -0400

    add file path

--- src/ContactList/FileHelper.java
+++ src/ContactList/FileHelper.java
@@ -1,7 +1,5 @@
 package ContactList;
 
-import com.google.gson.Gson;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,30 +23,30 @@ public class FileHelper {
             if (!Files.exists(dataFile)){
                 Files.createFile(dataFile);
             }
-            return dataFile
+            return dataFile;
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
         return null;
     }
 
-    public static void tryWriteFile(Path filePath, List<Person> personsToWrite){
-        try {
-            String personStr = new Gson().toJson(personsToWrite);
-            Files.write(filePath, Stream.of(personStr).toList());
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public static List<Person> tryReadFile(Path filePath) {
-        try {
-            return Stream
-                    .of(new Gson().fromJson(Files.readAllLines(filePath).get(0), Person[].class))
-                    .toList();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        return null;
-    }
+//    public static void tryWriteFile(Path filePath, List<Person> personsToWrite){
+//        try {
+//            String personStr = new Gson().toJson(personsToWrite);
+//            Files.write(filePath, Stream.of(personStr).toList());
+//        } catch (IOException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//    }
+//
+//    public static List<Person> tryReadFile(Path filePath) {
+//        try {
+//            return Stream
+//                    .of(new Gson().fromJson(Files.readAllLines(filePath).get(0), Person[].class))
+//                    .toList();
+//        } catch (IOException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//        return null;
+//    }
 }
--- src/ContactList/Main.java
+++ src/ContactList/Main.java
@@ -1,7 +1,19 @@
 package ContactList;
 
-public class Main {
-    public static void main(String[] args){
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        Path newPath = FileHelper.tryMakeFileDirectory();
+
+        Path YouthGroupPath = Paths.get("data", "package.json");
+        List<String> groceryList = Files.readAllLines(YouthGroupPath);
     }
 }
